3b42150070da9b4edf86ba5df456871ccd6dccca
dmathieu <42@dmathieu.com>
2009-11-10 13:11:39 +0100

    Loading all projects' god configs

--- god.rb
+++ god.rb
@@ -4,7 +4,7 @@
 # There's no management here. Only inclusions
 #
 
-ROOT = File.join(File.dirname(__FILE__), '..')
+ROOT = File.join(File.dirname(__FILE__))
 $: << File.dirname(__FILE__)
 
 #
@@ -17,4 +17,15 @@ God.contact(:email) do |c|
   c.email = '42@dmathieu.com'
 end
 
+#
+# The global recipes
+#
 require 'lib/apache'
+
+#
+# And in every project, we look for a potential god.rb file in the "god" directory and load it
+# But we ignore the current directory
+#
+Dir[ROOT + '/../*/'].each do |plugin|
+  load(File.join(plugin, 'god', 'god.rb')) if File.basename(plugin) != 'god' and File.directory?(plugin) and File.exists?(File.join(plugin, 'god', 'god.rb'))
+end
