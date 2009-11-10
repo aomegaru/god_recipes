3e7e9d8f4efd166dfdd02a7ac54e9d8481ce960f
dmathieu <42@dmathieu.com>
2009-11-10 12:21:50 +0100

    Adding notification

--- god.rb
+++ god.rb
@@ -7,4 +7,14 @@
 ROOT = File.join(File.dirname(__FILE__), '..')
 $: << File.dirname(__FILE__)
 
+#
+# Email notification settings
+#
+God::Contacts::Email.message_settings = { :from => 'god@predicteo.com' }
+God::Contacts::Email.server_settings = { :address => 'localhost', :port => 25 }
+God.contact(:email) do |c|
+  c.name = 'damien'
+  c.email = '42@dmathieu.com'
+end
+
 require 'lib/apache'
